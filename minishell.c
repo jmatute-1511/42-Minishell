bf66cfd5b4c619c0b2739983e33335b802c71ca5
PotatomixS <45759240+PotatomixS@users.noreply.github.com>
2022-07-13 12:31:42 +0200

    Signals pulidas

--- minishell.c
+++ minishell.c
@@ -14,14 +14,10 @@
 
 void	signal_handler(int signum)
 {
-	
-	if (signum == SIGINT && g_proc > 0)
+	if (signum == SIGINT && g_proc != 0)
 	{
 		kill(g_proc, SIGCONT);
-		// write(0, "\n", 1);
-		// rl_on_new_line();
-		// rl_replace_line("", 0);
-		// rl_redisplay();
+		write(0, "\n", 1);
 	}
 	else if (signum == SIGINT && g_proc == 0)
 	{
@@ -30,15 +26,7 @@ void	signal_handler(int signum)
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
-	else if (signum == SIGINT && g_proc < 0)
-	{
-		// write(0, "\n", 1);
-		// rl_on_new_line();
-		// rl_replace_line("", 0);
-		// rl_redisplay();
-	}
 }
-
 
 int main(int argc,char **argv,char **envp)
 {
@@ -54,6 +42,7 @@ int main(int argc,char **argv,char **envp)
 	myvars = start_vars(myvars,envp);
 	while (1)
 	{
+		g_proc = 0;
 		str = NULL;
 		str = readline(ROJO_T"Myshell%---->"COLOR_RESET);
 		if (str == NULL)
@@ -65,7 +54,6 @@ int main(int argc,char **argv,char **envp)
 			add_history(str);
 		if (init_nodes(&lst, &myvars, str) == 0)
 		{
-			//print_cmd(&lst);
 			if(lst)
 				execute_cmds(&lst, &myvars);
 		}
